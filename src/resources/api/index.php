5ea397d15db9d4d07ad432fa5d2a9aaa3b4dd7b5
Sara Hasan <sarahasan2270@gmail.com>
2026-05-20 18:05:17 +0300

    update

--- src/resources/api/index.php
+++ src/resources/api/index.php
@@ -78,11 +78,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 // TODO: Include the database connection file
-require_once __DIR__ . '/config/Database.php';
+require_once __DIR__ . '/../../common/db.php';
 
 // TODO: Get the PDO database connection
-$database = new Database();
-$db = $database->getConnection();
+$db = getDBConnection();
 
 
 // TODO: Get the HTTP request method
